159fc383b7e058726e5b8f9547268a7cb891d0ec
DominicDev <s0l0estudi4r@gmail.com>
2025-12-12 11:13:06 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LOS MORROCHUCOS';
+export let nameSongs = [
+	"CUANDO LLORA MI GUITARRA", /*1*/
+	"ALMA CORAZÓN Y VIDA", /*2*/
+	"LA FLOR DE LA CANELA", /*3*/
+	"TODOS VUELVEN", /*4*/
+	"ANITA", /*5*/
+	"HERMELINDA", /*6*/
+	"NUBE GRIS", /*7*/
+	"EL PLEBEYO", /*8*/
+	"ESTRELLITA DEL SUR", /*9*/
+	"HISTORIA DE MI VIDA", /*10*/
+	"EL HUERTO DE MI AMADA", /*11*/
+	"AMARRADITOS", /*12*/
+	"DÉJALOS", /*13*/
+	"SECRETO", /*14*/
+	"JACOBO EL LEÑADOR", /*15*/
+	"QUÉ IMPORTA", /*16*/
+	"CLAVEL MARCHITO", /*17*/
+	"MALDITO AMOR", /*18*/
+	"LLÁMAME", /*19*/
+	"JOSÉ ANTONIO", /*20*/
+]
